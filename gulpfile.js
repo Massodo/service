b383d14d232cb1b780783c7ecf19070b0bf883eb
Sergey <massodo1993@gmail.com>
2018-10-23 01:02:41 +0700

    Changed guplfile. Complited base markup. Added base, header and nav styles.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'), // Подключаем Gulp
     sass = require('gulp-sass'); // Подключаем Sass пакет
 
 gulp.task('sass', function() { // Создаем таск "sass"
-  return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
+  return gulp.src(['sass/main.scss']) // Берем источник
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('css')) // Выгружаем результата в папку css
   });
